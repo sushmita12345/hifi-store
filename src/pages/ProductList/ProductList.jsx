44d8476e0320f91ba5149ad63820e34f12d53990
sushmita12345 <38626023+sushmita12345@users.noreply.github.com>
2022-03-21 16:35:45 +0530

    style refactoring

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,5 +1,19 @@
+import {useEffect} from "react";
+import "./ProductList.css";
+import "../../assets/css/reset.css"
+
+
+
+import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+import { FinalFilterProduct } from "../../components/Sidebar/FinalFilterProduct/FinalFilterProduct";
+
+
 export function ProductList() {
-    return(
-        <h1>ProductList</h1>
-    )
+ 
+  return (
+    <div className="wrapper">
+      <Sidebar />
+      <FinalFilterProduct />
+    </div>
+  );
 }
